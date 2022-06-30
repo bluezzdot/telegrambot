66f7b498dc79003d43d5884067032fabbb868754
bluezdot <truong.pham@icetea.io>
2022-06-30 14:23:27 +0700

    fix wrong notify

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ def greet(message):
 
 @bot.message_handler(commands = ['help'])
 def help(message):
-    msg = "This is the list of commands: \n greet - Say hello to bot \n start - Introduction \n help - How to use this bot \n gettoken - Get some testnet token from Firebird"
+    msg = "This is the list of commands: \n /greet - Say hello to bot \n /start - Introduction \n /help - How to use this bot \n /faucet YOUR_ADDRESS - Get some testnet token from Firebird"
     print(f"{message.from_user.username}: {message.text}")
     print(f"Bot: {msg}")
-    bot.send_message(message.chat.id, "This is the list of commands: \n greet - Say hello to bot \n start - Introduction \n help - How to use this bot \n gettoken - Get some testnet token from Firebird")
+    bot.send_message(message.chat.id, msg)
 
 @bot.message_handler(commands = ['faucet'])
 def faucet(message):
-    msg = "Okey! I have your address now ! Please wait while I send you some tokens"
+    msg = "Okey! Please wait while I send you some tokens"
     print(f"{message.from_user.username}: {message.text}")
     print(f"Bot: {msg}")
     
@@ -40,16 +40,20 @@ def faucet(message):
         msg = "Invalid address! Please try again"
         print(f"Bot: {msg}")
         bot.send_message(message.chat.id, msg)
-        return
-    # send_eth(address) # MẤT TIỀN
-    msg2 = "Successfully sent some tokens to your address !"
-    bot.send_message(message.chat.id, msg2)
-    print(f"Bot: {msg2}")
+        
+    else:
+        msg = "I have your address now ! Please wait while I send you some tokens"
+        print(f"Bot: {msg}")
+        bot.send_message(message.chat.id, msg)
+        # send_eth(address) # MẤT TIỀN
+        msg2 = "Successfully sent some tokens to your address !"
+        bot.send_message(message.chat.id, msg2)
+        print(f"Bot: {msg2}")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     print(f"{message.from_user.username}: {message.text}")
-    print(f"Bot: Non")
-    bot.reply_to(message, "Non")
+    print(f"Bot: I don't understand you. You can type /help to see the list of commands")
+    bot.reply_to(message, "I don't understand you. You can type /help to see the list of commands")
 
 bot.infinity_polling()
